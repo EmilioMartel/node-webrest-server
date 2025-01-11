6606d7652ea7049ee4cb8ecdb1e479f31057278f
Emilio Martel <emilio_martel@outlook.es>
2025-01-11 20:13:36 +0000

    CRUD con prisma

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,75 +1,80 @@
 import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
 
-
-const todos = [
-  { id: 1, text: 'Buy milk', completedAt: new Date() },
-  { id: 2, text: 'Buy bread', completedAt: null },
-  { id: 3, text: 'Buy butter', completedAt: new Date() },
-];
 
 export class TodosController {
 
   //* DI
   constructor() {}
 
-  public getTodos = (req: Request, res: Response) => {
+  public getTodos = async (req: Request, res: Response) => {
+    const todos = await prisma.todo.findMany();
     return res.json(todos);
   };
 
-  public getTodoById = (req: Request, res: Response) => {
+  public getTodoById = async (req: Request, res: Response) => {
     const id = +req.params.id;
     if (isNaN(id)) return res.status(400).json({ error: `ID: ${id} is not a nubmer`})
-    const todo = todos.find (todo => todo.id === id);
+    
+      const todo = await prisma.todo.findFirst({
+      where: { id }
+    });
 
     ( todo )
       ? res.json(todo)
       : res.status(404).json({ error: `TODO with id ${id} not found`})
   }
 
-  public createTodo = (req: Request, res: Response) => {
-
+  public createTodo = async (req: Request, res: Response) => {
     const { text } = req.body
-
     if( !text ) res.status(400).json({ error: 'Text property is requiered'});
 
-    const newTodo = {
-      id: todos.length + 1,
-      text: text,
-      completedAt: null
-    }
+    const todo = await prisma.todo.create({
+      data: { text }
+    });
 
-    todos.push(newTodo);
-
-    res.json(newTodo)
+    res.json( todo );
   }
 
 
-  public updateTodo = (req: Request, res: Response) => {
+  public updateTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
     if (isNaN(id)) return res.status(400).json({ error: `ID: ${id} is not a nubmer`})
     
-    const todo = todos.find (todo => todo.id === id);
+    const todo = await prisma.todo.findFirst({
+      where: { id }
+    });
     if (!todo) return res.status(404).json({ error: `Todo with id: ${id} not found`});
 
     const { text, completedAt } = req.body;
     
-    todo.text = text || todo.text;
-    ( completedAt === 'null' )
-      ? todo.completedAt = null
-      : todo.completedAt = new Date( completedAt || todo.completedAt );
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { 
+        text,
+        completedAt: (completedAt) ? new Date(completedAt) : null
+      }
+    });
 
-    res.json(todo);
+    res.json(updatedTodo);
   }
 
-  public deleteTodo = (req: Request, res: Response) => {
+  public deleteTodo = async (req: Request, res: Response) => {
 
     const id = +req.params.id;
     if (isNaN(id)) return res.status(400).json({ error: `ID: ${id} is not a nubmer`})
 
-    const todo = todos.find( todo => todo.id === id);
+    const todo = await prisma.todo.findFirst({
+      where: { id }
+    });
     if (!todo) return res.status(404).json({ error: `Todo with id: ${id} not found`})
-    todos.splice( todos.indexOf(todo), 1);
-    res.json(todo);
+
+    
+    const deleted = await prisma.todo.delete({
+      where: { id }
+    })
+
+    res.json({ todo, deleted });
     
 
   }
